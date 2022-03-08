27b48514100a4e8788316eb27bcd2762a0ef5954
vasiliy-klimkin <vasily.klimkin@gmail.com>
2022-03-08 12:23:13 -0500

    refactor to a function

--- problem/addNumbers.js
+++ problem/addNumbers.js
@@ -5,23 +5,16 @@
 // 1,2,3,4,5 <
 // 15 >
 
-// How do I get command Line arguments?
-// console.log(process.argv);
-const arguments = process.argv;
-// How to Slice value to get rid of first 2 elements ?
-console.log("-------------------------------");
 // splice, slice
-const numbers = arguments.slice(2);
-console.log(numbers);
-console.log('#####################3');
-let sum = 0;
-for (let i = 0; i < numbers.length; i++) {
-    // How do I add numbers toghether ?
-    console.log("i ", i);
-    console.log("numbers[i] ",numbers[i]);
-    sum = sum + Number(numbers[i]);
+const addNumbers = function() {
+    const arguments = process.argv;
+    const numbers = arguments.slice(2);
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+        // How do I add numbers toghether ?
+        sum = sum + Number(numbers[i]);
+    }    
+    console.log("RESULT !!!!!", sum);
 }
 
-console.log("RESULT !!!!!", sum);
-// once command line arguments have come back...
-// How do I add multiple Numbers toghether (where size of elements is different) ?
+addNumbers();
